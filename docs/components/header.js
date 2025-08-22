8483caacbf35e870d72be0611cb31390ac68831c
PastelDePaoCaique <caique.silva34@fatec.sp.gov.br>
2025-08-21 22:19:06 -0300

    fiz a pasta components e fiz o header

---
+++ docs/components/header.js
@@ -0,0 +1,8 @@
+document.getElementById("header").innerHTML = `
+<navbar class="navbar">
+  <img src="static/img/logo.png" alt="Logo" class="logo_navbar" />
+  <div class="lista_link_navbar">
+    <a class="link_navbar" href="index.html">HOME</a>
+    <a class="link_navbar" href="projetos.html">PROJETOS</a>
+  </div>
+</navbar>`;
